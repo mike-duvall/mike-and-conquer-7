a3994f4ae935d20d947148a11dc58d98d7519b1e
Mike DuVall <mike.a.duvall@gmail.com>
2018-02-20 07:36:24 -0600

    Mouse click from test working

--- mike-and-conquer/rest/LeftClickController.cs
+++ mike-and-conquer/rest/LeftClickController.cs
@@ -1,19 +1,66 @@
 ﻿
+using System.Runtime.InteropServices;
 using System.Web.Http;
 
 
+using IntPtr = System.IntPtr;
 
-
-
-using WindowsInput.Native;
-using WindowsInput;
 
 namespace mike_and_conquer.rest
 {
 
 
+
     public class LeftClickController : ApiController
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+
+        struct INPUT
+        {
+            public int type;
+            public MOUSEKEYBDHARDWAREINPUT mkhi;
+        }
+
 
 
         //[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
@@ -35,16 +82,47 @@ namespace mike_and_conquer.rest
         //    mouse_event(MOUSEEVENTF_LEFTUP, mouseX, mouseY, 0, 0);
         //}
 
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
         public void DoMouseClick(uint mouseX, uint mouseY)
         {
-            InputSimulator sim = new InputSimulator();
 
-            sim.Mouse.MoveMouseTo(mouseX, mouseY);
-            sim.Mouse.Sleep(1000);
-            sim.Mouse.MoveMouseTo(mouseX + 200, mouseY + 200);
-            sim.Mouse.LeftButtonClick();
+            INPUT x = new INPUT();
+            x.type = 0;
+            //x.mkhi.mi.dx = 564;
+            //x.mkhi.mi.dy = 425;
+            x.mkhi.mi.dx = (int)mouseX;
+            x.mkhi.mi.dy = (int)mouseY;
 
-            Pickup here using ::SendInput like in old C++ version
+            x.mkhi.mi.mouseData = 0;
+            //x.mkhi.mi.dwFlags = 0x01;
+            x.mkhi.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            x.mkhi.mi.time = 1000;
+//            x.mkhi.mi.dwExtraInfo = GetMessageExtraInfo();
+            int size = Marshal.SizeOf(x);
+            uint y = SendInput(1, ref x, size);
+
+            System.Threading.Thread.Sleep(1000);
+
+            x.mkhi.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            uint y2 = SendInput(1, ref x, size);
+
+
+
+
+            //        mouseInput.mi.dwFlags = MOUSEEVENTF_LEFTDOWN; ;
+
+            //        ::SendInput(1, &mouseInput, sizeof(INPUT));
+
+            //        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            //        mouseInput.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+            //::SendInput(1, &mouseInput, sizeof(INPUT));
+
+
         }
 
 

f6e0d2a44389ad6c250fe8a096bea8682b4dd573
mike-duvall <mike.a.duvall@gmail.com>
2020-04-18 10:30:35 -0500

    Rename

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mike-and-conquer-6")]
-[assembly: AssemblyProduct("mike-and-conquer-6")]
+[assembly: AssemblyTitle("mike-and-conquer")]
+[assembly: AssemblyProduct("mike-and-conquer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Microsoft")]

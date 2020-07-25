514ac35165a56ed64ecfe197427617748bd8acab
mike-duvall <mike.a.duvall@gmail.com>
2020-07-25 09:42:29 -0500

    Multiple placement tiles, kind of working

--- src/gameview/BarracksPlacementIndicatorView.cs
+++ src/gameview/BarracksPlacementIndicatorView.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using mike_and_conquer.gamesprite;
 using mike_and_conquer.gameworld;
 using mike_and_conquer.main;
+using mike_and_conquer.util;
 using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
 
 namespace mike_and_conquer.gameview
@@ -34,15 +35,21 @@ namespace mike_and_conquer.gameview
 
         internal void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            Point placementSquarePosition = this.position;
+            DrawPlacementSquareAtPosition(placementSquarePosition, gameTime, spriteBatch);
+            placementSquarePosition.X = (placementSquarePosition.X + GameWorld.MAP_TILE_WIDTH);
+            DrawPlacementSquareAtPosition(placementSquarePosition, gameTime, spriteBatch);
+        }
 
-            Vector2 position = new Vector2(this.position.X, this.position.Y);
 
-            if (GameWorld.instance.IsValidBuildingPlacementLocation(this.position))
+        void DrawPlacementSquareAtPosition(Point position, GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (GameWorld.instance.IsValidBuildingPlacementLocation(position))
             {
                 canPlaceBuildingSprite.Draw(
                     gameTime,
                     spriteBatch,
-                    position,
+                    PointUtil.ConvertPointToVector2(position),
                     SpriteSortLayers.MAP_SQUARE_DEPTH,
                     false,
                     Color.White);
@@ -53,18 +60,14 @@ namespace mike_and_conquer.gameview
                 canNotPlaceBuildingSprite.Draw(
                     gameTime,
                     spriteBatch,
-                    position,
+                    PointUtil.ConvertPointToVector2(position),
                     SpriteSortLayers.MAP_SQUARE_DEPTH,
                     false,
                     Color.White);
 
             }
 
-
         }
-
-
-
 
 
 
